7cee353f5067d1fa3f5525ee3c0017efb7e939ee
Desmond Rgwaringesu <desmondrg@gmail.com>
2021-11-16 03:27:21 +0200

    Fixed hide on scroll transition

--- src/shared/components/layouts/AppLayout.tsx
+++ src/shared/components/layouts/AppLayout.tsx
@@ -32,27 +32,19 @@ interface Props {
 
 function HideOnScroll(props: Props) {
     const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
-    const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
-    });
+    const trigger = useScrollTrigger({threshold: 5});
 
     return (
-        <Slide appear={false} direction="down" in={!trigger}>
+        <Slide appear={false} direction="down" in={!trigger} timeout={{appear: 100, enter: 100, exit: 100}}>
             {children}
         </Slide>
     );
 }
 
 function ScrollTop(props: Props) {
-    const { children, window } = props;
-    // Note that you normally won't need to set the window ref as useScrollTrigger
-    // will default to window.
-    // This is only being set here because the demo is in an iframe.
+    const { children } = props;
+
     const trigger = useScrollTrigger({
-        target: window ? window() : undefined,
         disableHysteresis: true,
         threshold: 100,
     });
@@ -104,7 +96,7 @@ export default function AppLayout(props: Props) {
             <CssBaseline />
             <HideOnScroll {...props}>
                 <Box id='back-to-top-anchor' sx={{ flexGrow: 1 }}>
-                    <AppBar position="absolute" style={{ background: 'transparent', boxShadow: 'none'}}>
+                    <AppBar position="fixed" style={{ background: 'transparent', boxShadow: 'none'}}>
 
                         <Toolbar>
                             <Link to='/'>
